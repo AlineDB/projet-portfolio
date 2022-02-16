40032feaa593098580d0a93c562ca1fd28337335
AlineDB <71900695+AlineDB@users.noreply.github.com>
2022-02-16 12:06:37 +0100

    init 2

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -6,3 +6,15 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 //activer les img sur les articles
 add_theme_support('post-thumbnails');
+
+//enregistrer un seul custom post type pour mes livres
+register_post_type('books', [
+    'label' => 'Livres',
+    'labels'=> [
+        'name' => 'Mes livres',
+    ],
+    'description' => 'Résumé des livres',
+    'menu_position' => 10,
+    'menu_icon' => 'dashicons-book',
+    'public' => true
+]);
