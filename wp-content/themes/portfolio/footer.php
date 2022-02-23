44c5d52e87c0753d37991e366ec2918c959d1726
AlineDB <71900695+AlineDB@users.noreply.github.com>
2022-02-23 16:26:20 +0100

    init 3

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -3,6 +3,7 @@
 <footer class="footer__">
     <h2 class="footer__title">Pied de page</h2>
     <a href="template-about.php">A propos</a>
+    <a href="template-contact.php">Contact</a>
 </footer>
 
 
